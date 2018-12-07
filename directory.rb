c20eb4c20017c79d6746ad01218fd5bc3dc07732
Erin Stevenson <estevenson_94@live.com>
2018-12-07 11:36:52 +0000

    Making the output look nice with center()

--- directory.rb
+++ directory.rb
@@ -2,36 +2,39 @@ def input_students
   puts "Please enter the name of the student"
   puts "If there are no more students, hit return twice"
   students = []
-  name = gets.chomp
+  name = gets.chomp.capitalize
   while !name.empty? do
     puts "Which cohort is this student part of?"
-    cohort = gets.chomp
+    cohort = gets.chomp.capitalize
     puts "What is their country of birth?"
-    birth_country = gets.chomp
+    birth_country = gets.chomp.capitalize
     puts "What is their favourite past time?"
     hobby = gets.chomp
     students << {name: name, cohort: cohort, birth_country: birth_country, hobby: hobby}
     puts "Now we have #{students.count} students"
     puts "Please enter name of student or hit return if you are done"
-    name = gets.chomp
+    name = gets.chomp.capitalize
   end
   students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(50)
+  puts "-------------".center(50)
 end
 def print(students)
   student_number = 0
   while student_number < students.length do
-    puts "#{student_number + 1}. #{students[student_number][:name]} (#{students[student_number][:cohort]} cohort)"
+    puts ""
+    puts "#{student_number + 1}. #{students[student_number][:name]} (#{students[student_number][:cohort]} cohort)".center(50)
     puts "#{students[student_number][:name]} was born in #{students[student_number][:birth_country]} and enjoys #{students[student_number][:hobby]} in their spare time"
+    puts ""
     student_number += 1    
   end
 end
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts ""
+  puts "Overall, we have #{names.count} great students".center(50)
 end
 
 students = input_students
