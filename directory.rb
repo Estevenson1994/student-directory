2efa0523f12380606760f4e42e9407cf4137c3c3
Erin Stevenson <estevenson_94@live.com>
2018-12-07 15:15:04 +0000

    Interactive cohort including default value and typos

--- directory.rb
+++ directory.rb
@@ -1,18 +1,28 @@
 def input_students
   puts "Please enter the name of the student"
-  puts "If there are no more students, hit return twice"
+  puts "If there are no more students, hit return"
   students = []
+  default_value = :default
+  cohorts = [:November, :January, :March, :May, :July, :September]
   name = gets.chomp.capitalize
   while !name.empty? do
-    puts "Which cohort is this student part of?"
-    cohort = gets.chomp.capitalize
+    puts "Which cohort is this student part of? If you're not sure, just hit enter"
+    cohort = gets.chomp.capitalize.to_sym
+    if cohort.empty?
+      cohort = default_value
+    else
+      while cohorts.include?(cohort) == false do
+        puts "That isn't an available cohort, please try again"
+        cohort = gets.chomp.capitalize.to_sym
+      end
+    end
     puts "What is their country of birth?"
     birth_country = gets.chomp.capitalize
     puts "What is their favourite past time?"
     hobby = gets.chomp
     students << {name: name, cohort: cohort, birth_country: birth_country, hobby: hobby}
     puts "Now we have #{students.count} students"
-    puts "Please enter name of student or hit return if you are done"
+    puts "Please enter name of student or hit return if there are no more"
     name = gets.chomp.capitalize
   end
   students
